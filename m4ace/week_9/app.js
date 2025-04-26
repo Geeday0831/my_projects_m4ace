e3fb88d5141a5ce78cec525bbe9383890def0fe7
abdulrauf <abdulraufabdulwahab3108@gmail.com>
2025-04-26 17:12:23 +0100

    assignment 9

---
+++ m4ace/week_9/app.js
@@ -0,0 +1,7 @@
+const rectangleArea = require('./rectangle');
+
+const width = 4;
+const height = 8;
+
+const area = rectangleArea(width, height);
+console.log(`The area of the rectangle is : ${area}`);
