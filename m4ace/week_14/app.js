3ebdf39d992433751c56e3f20f0b6d850d57163f
Geeday0831 <abdulraufabdulwahab3108@gmail.com>
2025-07-14 22:34:04 +0100

    week_15, midified week 14 to get my new assignment

--- m4ace/week_14/app.js
+++ m4ace/week_14/app.js
@@ -1,11 +1,30 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+const User = require('./models/user');
 
-const url = 'mongodb+srv://abdulwahabjide:MzE733yg2ewc0lcV@cluster0.0qg0nxj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
+
+const url = process.env.MONGODB_URI;
 
 mongoose.connect(url)
-  .then(() => {
+  .then(async () => {
     console.log('Connected to MongoDB with Mongoose');
-    // You can define schemas and models here
+
+    // Add a user
+    const newUser = new User({
+      name: 'John Doe',
+      email: 'john@example.com',
+      age: 30
+    });
+
+    await newUser.save();
+    console.log('User added:', newUser);
+
+    //to fetch and print all my users
+    const users = await User.find();
+    console.log('All users:', users);
+
+    mongoose.connection.close();
   })
   .catch((err) => {
     console.error('Failed to connect to MongoDB', err);
